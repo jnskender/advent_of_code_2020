9ec8cd880ded8f2cd097c4cda37cce1e9b0b81dd
jnskender <jnskender@gmail.com>
2020-12-04 15:56:42 -0600

    finish day 4 part 2

--- day-4/index.js
+++ day-4/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('allValid.txt', 'utf8', (error, data) => {
+fs.readFile('puzzle.txt', 'utf8', (error, data) => {
 
   const passports = data.toString().split("\n\n");
 
@@ -19,25 +19,14 @@ fs.readFile('allValid.txt', 'utf8', (error, data) => {
 
 
   const partTwo = () => {
-    const requirements = [
-      /byr:19[2-9][0-9]|20[0-2]0/g,
-      /iyr:201[0-9]|iyr:20[0-2]0/g,
-      /eyr:20[0-2][0-9]|iyr:2030/g,
-      /hgt:\d*(cm|in)/g,
-      /hcl:#([0-9]|[a-f]){6}/g,
-      /ecl:(amb|blu|brn|gry|grn|hzl|oth)/g,
-      /pid:[0-9]{9}/g
-    ];
-
-    const r = /(byr:19[2-9][0-9]|20[0-2]0)|(iyr:201[0-9]|iyr:20[0-2]0)|(eyr:20[0-2][0-9]|iyr:2030)|(hgt:\d*(cm|in))|(hcl:#([0-9]|[a-f]){6})|(ecl:(amb|blu|brn|gry|grn|hzl|oth))|(pid:[0-9]{9})/
-    // const valid = passports.reduce((acc, v) => {
-    //   if (v.match(r).length === 8 ) {
-    //     return acc + 1;
-    //   }
-    //   return acc;
-    // }, 0);
-
-    return passports[1].match(r);
+    const r = /(byr:(19[2-9][0-9]|200[0-2]))|(iyr:(201[0-9]|2020))|(eyr:(202[0-9]|2030))|(hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in))|(hcl:#(([0-9]|[a-f]){6}))|(ecl:(amb|blu|brn|gry|grn|hzl|oth){1})|(pid:[0-9]{9}(?!\w))/g
+    const valid = passports.reduce((acc, v) => {
+      if (v.match(r) && v.match(r).length === 7) {
+        return acc + 1;
+      }
+      return acc
+    }, 0);
+    return valid
   }
 
   console.log(partTwo());
